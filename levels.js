f184f20275f5f9d6a1406f55ebdce8151ab04174
soosoohan <gkstmxpffk@naver.com>
2026-07-31 15:56:03 +0900

    Update levels.js

--- levels.js
+++ levels.js
@@ -25,7 +25,35 @@ const LEVELS = {
       { pos: [2,1], value: 32 }
     ],
     deck: [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
-  }
+  },
+  nstamp_004: {
+  grid: { rows: 3, cols: 3 },
+  rocks: [
+    { pos: [0,0], value: 8 },
+    { pos: [1,1], value: 16 },
+    { pos: [2,2], value: 8 }
+  ],
+  deck: [2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4]
+},
+nstamp_005: {
+  grid: { rows: 3, cols: 3 },
+  rocks: [
+    { pos: [0,1], value: 16 },
+    { pos: [1,0], value: 8 },
+    { pos: [1,2], value: 8 }
+  ],
+  deck: [4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,2,4,2]
+},
+nstamp_006: {
+  grid: { rows: 3, cols: 3 },
+  rocks: [
+    { pos: [0,0], value: 32 },
+    { pos: [2,0], value: 8 },
+    { pos: [2,2], value: 16 }
+  ],
+  deck: [2,2,4,4,2,2,4,4,2,2,4,4,2,2,4,4,2,2,4,4,2,2,4,4]
+}
+
 };
 
 const LEVEL_IDS = Object.keys(LEVELS);
