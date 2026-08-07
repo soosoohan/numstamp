85a2a064decf23881cb77293363c1a8d2d93f12c
soosoohan <gkstmxpffk@naver.com>
2026-08-07 10:44:30 +0900

    Update levels.js

--- levels.js
+++ levels.js
@@ -79,7 +79,120 @@ nstamp_009: {
     { pos: [3,1], value: 32 }
   ],
   deck: [4,2,8,4,2,8,4,2,8,4,2,8,4,2,8,4,2,8,4,2,8,4,2,8,4,2]
-}
+},
+    nstamp_010: {
+    grid: { rows: 4, cols: 4 },
+    rocks: [
+      { pos: [3,1], value: 8 },
+      { pos: [2,1], value: 16 },
+      { pos: [3,2], value: 8 },
+      { pos: [1,3], value: 32 }
+    ],
+    deck: [2,4,8,2,4,8,2,4,8,2,4,8,2,4,8,2,4,8,2,4,8,2,4,8]
+  },
+  nstamp_011: {
+    grid: { rows: 4, cols: 4 },
+    rocks: [
+      { pos: [3,0], value: 16 },
+      { pos: [3,3], value: 16 },
+      { pos: [2,1], value: 8 },
+      { pos: [2,2], value: 8 }
+    ],
+    deck: [4,2,8,4,2,8,4,2,8,4,2,8,4,2,8,4,2,8,4,2,8,4,2,8]
+  },
+  nstamp_012: {
+    grid: { rows: 4, cols: 4 },
+    rocks: [
+      { pos: [1,1], value: 32 },
+      { pos: [2,0], value: 8 },
+      { pos: [3,3], value: 8 },
+      { pos: [3,1], value: 16 }
+    ],
+    deck: [2,2,4,8,2,2,4,8,2,2,4,8,2,2,4,8,2,2,4,8,2,2,4,8]
+  },
+  nstamp_013: {
+    grid: { rows: 4, cols: 5 },
+    rocks: [
+      { pos: [3,0], value: 8 },
+      { pos: [3,4], value: 8 },
+      { pos: [2,2], value: 16 },
+      { pos: [1,3], value: 8 }
+    ],
+    deck: [2,4,8,4,2,4,8,4,2,4,8,4,2,4,8,4,2,4,8,4,2,4,8,4]
+  },
+  nstamp_014: {
+    grid: { rows: 4, cols: 5 },
+    rocks: [
+      { pos: [3,1], value: 16 },
+      { pos: [3,3], value: 16 },
+      { pos: [2,0], value: 8 },
+      { pos: [2,4], value: 8 },
+      { pos: [1,2], value: 32 }
+    ],
+    deck: [4,4,2,8,4,4,2,8,4,4,2,8,4,4,2,8,4,4,2,8,4,4,2,8]
+  },
+  nstamp_015: {
+    grid: { rows: 5, cols: 3 },
+    rocks: [
+      { pos: [4,0], value: 8 },
+      { pos: [4,2], value: 8 },
+      { pos: [3,1], value: 16 }
+    ],
+    deck: [2,4,8,2,4,8,2,4,8,2,4,8,2,4,8,2,4,8]
+  },
+  nstamp_016: {
+    grid: { rows: 5, cols: 3 },
+    rocks: [
+      { pos: [4,1], value: 32 },
+      { pos: [3,0], value: 8 },
+      { pos: [3,2], value: 8 },
+      { pos: [2,1], value: 16 }
+    ],
+    deck: [4,2,2,8,4,2,2,8,4,2,2,8,4,2,2,8,4,2,2,8]
+  },
+  nstamp_017: {
+    grid: { rows: 5, cols: 4 },
+    rocks: [
+      { pos: [4,0], value: 16 },
+      { pos: [4,3], value: 16 },
+      { pos: [3,1], value: 8 },
+      { pos: [3,2], value: 8 }
+    ],
+    deck: [2,4,8,2,4,8,2,4,8,2,4,8,2,4,8,2,4,8,2,4,8,2,4,8]
+  },
+  nstamp_018: {
+    grid: { rows: 5, cols: 4 },
+    rocks: [
+      { pos: [4,1], value: 8 },
+      { pos: [4,2], value: 8 },
+      { pos: [3,0], value: 16 },
+      { pos: [3,3], value: 16 },
+      { pos: [2,1], value: 32 }
+    ],
+    deck: [4,2,8,4,2,8,4,2,8,4,2,8,4,2,8,4,2,8,4,2,8,4,2,8]
+  },
+  nstamp_019: {
+    grid: { rows: 5, cols: 5 },
+    rocks: [
+      { pos: [4,0], value: 8 },
+      { pos: [4,4], value: 8 },
+      { pos: [3,2], value: 16 },
+      { pos: [2,1], value: 8 },
+      { pos: [2,3], value: 8 }
+    ],
+    deck: [2,4,8,2,4,8,2,4,8,2,4,8,2,4,8,2,4,8,2,4,8,2,4,8,2,4,8,2,4,8]
+  },
+  nstamp_020: {
+    grid: { rows: 5, cols: 5 },
+    rocks: [
+      { pos: [4,1], value: 16 },
+      { pos: [4,3], value: 16 },
+      { pos: [3,0], value: 8 },
+      { pos: [3,4], value: 8 },
+      { pos: [1,2], value: 32 }
+    ],
+    deck: [4,4,2,8,4,4,2,8,4,4,2,8,4,4,2,8,4,4,2,8,4,4,2,8,4,4,2,8]
+  }
 
 
 };
